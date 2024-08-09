b0934d3831723bd834a52efe800a40875c60d963
Arda Cihat Karahan <ardacihatkarahan@gmail.com>
2024-08-09 15:10:40 +0300

    Added GameFiles

---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 10f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xValue = Input.GetAxis("Horizontal")*Time.deltaTime*moveSpeed;
+        float zValue = Input.GetAxis("Vertical")*Time.deltaTime*moveSpeed;
+        transform.Translate(xValue,0,zValue);
+    }
+}
